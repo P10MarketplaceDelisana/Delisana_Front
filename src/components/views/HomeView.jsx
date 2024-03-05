a8a99b68b4be73a9ab39548c5fa0e1ed7f6ec390
Claudia González <claudia.glezg@gmail.com>
2024-03-05 15:50:41 +0100

    add basic estructure and change colors

--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../molecules/Navbar'
 
 const HomeView = () => {
   return (
+    <>
+    <Navbar />
     <div>HomeView</div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,20 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brown-letter': '#330F0F',
+        'beige-bg': '#f3ece3',
+        'granate': '#AC0231',
+        'blue-sky': '#88CAD6',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
